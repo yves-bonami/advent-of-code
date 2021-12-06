5a122035fe24ce5ad6c6321d7c0823233876cabe
Yves Bonami <yves.bonami@gmail.com>
2021-12-06 10:16:50 +0100

    Solve Day One - Part Two

--- examples/day-one.rs
+++ examples/day-one.rs
@@ -1,14 +1,13 @@
 use advent_of_code::DayOne;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input = include_str!("./day-one.txt")
+    let input: Vec<i32> = include_str!("./day-one.txt")
         .split("\n")
-        .map(|n| {
-            n.trim().parse::<i32>().unwrap()
-        })
+        .map(|n| n.trim().parse::<i32>().unwrap())
         .collect();
 
-    println!("Day 1 - Part 1: {}", DayOne::part_one(input));
+    println!("Day 1 - Part 1: {}", DayOne::part_one(input.clone()));
+    println!("Day 1 - Part 2: {}", DayOne::part_two(input.clone()));
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,26 @@ impl DayOne {
                 count += 1;
             }
         }
-
         count
+    }
+
+    pub fn part_two(input: Vec<i32>) -> i32 {
+        let mut count = 0;
+        let windows = DayOne::get_windows(input);
+        for i in 1..windows.len() {
+            if windows[i] > windows[i - 1] {
+                count += 1;
+            }
+        }
+        count
+    }
+
+    fn get_windows(input: Vec<i32>) -> Vec<i32> {
+        let mut windows = Vec::new();
+        for i in 0..input.len() - 2 {
+            windows.push(input[i] + input[i + 1] + input[i + 2]);
+        }
+        windows
     }
 }
 
@@ -21,5 +39,11 @@ mod tests {
     fn test_day_one() {
         let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
         assert_eq!(DayOne::part_one(input), 7);
+    }
+
+    #[test]
+    fn test_day_two() {
+        let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(DayOne::part_two(input), 5);
     }
 }
